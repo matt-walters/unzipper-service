d40c38e80eff404c7ca107e5ee5f1815656bd927
matt-walters <mgwalters81@gmail.com>
2019-07-11 22:16:45 +1000

    Added option to delete files after they're unzipped. Added content to the readme file.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # UnzipperService
 
+A Windows service that automatically extracts any zip files that placed in a specified folder.
+The service is built with [TopShelf](http://topshelf-project.com/) which makes installing and starting the service super easy.
+
+## Install
+
+`UnzipperService.exe install`
+
+## Start
+
+`UnzipperService.exe start`
+
+## Stop
+
+`UnzipperService.exe stop`
+
+## Uninstall
+
+`UnzipperService.exe uninstall`
---
+++ src/UnzipperService/UnzipperConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+
+namespace UnzipperService
+{
+    public class UnzipperConfig
+    {
+        public string SourceFolder { get; private set; }
+        public string DestinationFolder { get; private set; }
+        public bool DeleteFileAfterUnzip { get; private set; }
+        public int WorkerTaskCount { get; private set; }
+
+        public static UnzipperConfig FromAppConfig()
+        {
+            return new UnzipperConfig()
+            {
+                SourceFolder = ConfigurationManager.AppSettings["sourceFolderPath"],
+                DestinationFolder = ConfigurationManager.AppSettings["destinationFolderPath"],
+                DeleteFileAfterUnzip = bool.Parse(ConfigurationManager.AppSettings["deleteFileAfterUnzip"]),
+                WorkerTaskCount = int.Parse(ConfigurationManager.AppSettings["workerTaskCount"])
+            };
+        }
+    }
+}
